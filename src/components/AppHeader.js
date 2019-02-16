89ee797a218f3f86b95d97d8d0bd547889b8b899
Hardik R <hardik_rathore@yahoo.com>
2019-02-17 01:12:15 +0530

    #rename directories
    #

---
+++ src/components/AppHeader.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+export default class AppHeader extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: "",
+      suggestionArray: []// array of suggestion key word
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+
+  }
+
+  handleChange(event) {
+
+    var query = event.target.value;
+    this.setState({ query: query }, () => {
+      // fetch suggestion api here
+    });
+
+  }
+  handleSubmit(event) {
+
+    var query = this.state.query;
+    query = query.trim();
+ 
+    if (event.key === 'Enter' && query.length > 0) {
+ 
+      this.props.queryString(query);
+
+    }
+  }
+  render() {
+    return (
+      <div style={
+        {
+          height: 50,
+          background: "#F0F0F0",
+          border: "opx solid #CCC",
+          minWidth: "100vW",
+          margin: "0px",
+          textAlign: "cetner",
+          position: "fixed",
+          top:0
+        }
+      }>
+        <input style={{
+          display: "flex",
+          minWidth: 300,
+          height: "80%",
+          margin: "0 auto",
+          fontSize: 16
+        }}
+          type="text" id="search" placeholder="search" value={this.state.query}
+          onChange={this.handleChange} onKeyPress={this.handleSubmit} />
+      </div>
+    )
+  }
+}
---
+++ src/views/Login.js
@@ -0,0 +1,97 @@
+import React, { Component } from 'react'
+import AppHeader from '../components/AppHeader';
+import cookie from 'react-cookies'
+export default class Login extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      userAuth: false,
+      username: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+
+  }
+
+
+  componentWillMount() {
+    var authCookieValue = cookie.load("auth");// should be replaced by auth token
+    if (authCookieValue === undefined) {
+      authCookieValue = false;
+    } else {
+      authCookieValue = JSON.parse(authCookieValue);
+    }
+    this.setState({ userAuth: authCookieValue }, () => {
+
+      //console.log("cookie value " + authCookieValue);
+      if (authCookieValue) { this.props.history.push("/search") };
+
+    });
+
+  }
+
+
+  handleChange(event) {
+    this.setState({ [event.target.id]: event.target.value }, () => {
+
+      // validation
+    })
+
+  }
+
+  handleSubmit() {
+
+    if (this.state.username === "admin" && this.state.password === "password") {
+       cookie.save("auth", true);
+      this.props.history.push("/search")
+    } else {
+       window.confirm('Invalid Credentials')    }
+  }
+  render() {
+    return (
+
+      <div >
+        <div style={{
+          minWidth: 320,
+          maxWidth: 500,
+          position: "absolute",
+          margin: "auto",
+          top: "20%",
+          right: 0,
+          bottom: 0,
+          left: 0,
+        }}>
+
+          <form>
+            <fieldset>
+              <legend>Login here</legend>
+              <label >Username</label>
+              <br />
+
+              <input style={{display:"table-cell", width:"100%"}} 
+              type="text" id="username" placeholder="Username" 
+              onChange={this.handleChange} value={this.state.username} />
+              <br />
+
+              <label >Password</label>
+              <br />
+
+              <input style={{display:"table-cell", width:"100%"}}
+               type="password" id="password" placeholder="Password"
+                onChange={this.handleChange} value={this.state.password} />
+              <br />
+
+              <input id="submit" type="button" value="Login" onClick={this.handleSubmit} />
+            </fieldset>
+          </form>
+        </div>
+
+      </div>
+    )
+  }
+}
