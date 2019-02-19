2cc4db90d3a77ee281f96fdd540609560ee14b60
Hardik R <hardik_rathore@yahoo.com>
2019-02-19 15:28:56 +0530

    mobx   in login page

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,74 +2,77 @@ import React, { Component } from 'react'
 //import AppHeader from '../components/AppHeader';
 import cookie from 'react-cookies'
 
-import { extendObservable } from 'mobx';
-import { observer } from 'mobx-react';
+import { observable } from 'mobx';
+  
+class loginStore {
+  @observable username = "";
+  @observable password ="";
+}
 
-@observer
   class Login extends Component {
 
-    constructor(props) {
-      super(props);
+  constructor(props) {
+    super(props);
 
-      extendObservable(this, {
-        username: '',
-        password: ''
-      });
+   
+  
 
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
 
+  }
+
+  componentWillMount() {
+    var authCookieValue = cookie.load("auth");// should be replaced by auth token
+    if (authCookieValue === undefined) {
+      authCookieValue = false;
+    } else {
+      authCookieValue = JSON.parse(authCookieValue);
     }
+    let userAuth = authCookieValue;
+    if (authCookieValue) { this.props.history.push("/search") };
 
-    componentWillMount() {
-      var authCookieValue = cookie.load("auth");// should be replaced by auth token
-      if (authCookieValue === undefined) {
-        authCookieValue = false;
-      } else {
-        authCookieValue = JSON.parse(authCookieValue);
-      }
-      let userAuth = authCookieValue;
-      if (authCookieValue) { this.props.history.push("/search") };
+  }
 
-    }
+  handleChange(event) {
+    const { id, value } = event.target;
+    this[id] = value;
+    console.log(this.username, value);
 
-    handleChange(event) {
-      const { id, value } = event.target;
-      this[id] = value;
-      console.log(this.username, value);
+  }
 
-    }
-
-    handleSubmit() {
-      const { username, password } = this;
-       // hard coded auth logic
-      if (username === "admin" && password === "password") {
-        cookie.save("auth", true);
-        this.props.history.push("/search")
-      } else {
-        window.confirm('Invalid Credentials')
-      }
-    }
-
-    render() {
-
-      const { username, password } = this;
-      return (
-
-        <div className="loginForm">
-          <form>
-            <fieldset>
-              <div className="form-group">
-                <label >Username</label>
-                <input type="text" id="username" placeholder="Username" onChange={this.handleChange} value={username} />
-                <label >Password</label>
-                <input type="password" id="password" placeholder="Password" onChange={this.handleChange} value={password} />
-                <input id="submit" type="button" value="Login" onClick={this.handleSubmit} />
-              </div>
-            </fieldset>
-          </form>
-        </div>
-
-      )
+  handleSubmit() {
+    const { username, password } = this;
+      // hard coded auth logic
+    if (username === "admin" && password === "password") {
+      cookie.save("auth", true);
+      this.props.history.push("/search")
+    } else {
+      window.confirm('Invalid Credentials')
     }
   }
+
+  render() {
+
+    const { username, password } = this;
+    return (
+
+      <div className="loginForm">
+        <form>
+          <fieldset>
+            <div className="form-group">
+              <label >Username</label>
+              <input type="text" id="username" placeholder="Username" onChange={this.handleChange} value={username} />
+              <label >Password</label>
+              <input type="password" id="password" placeholder="Password" onChange={this.handleChange} value={password} />
+              <input id="submit" type="button" value="Login" onClick={this.handleSubmit} />
+            </div>
+          </fieldset>
+        </form>
+      </div>
+
+    )
+  }
+}
+
+export default Login;
--- src/views/Login.test.js
+++ src/views/Login.test.js
@@ -2,15 +2,14 @@ import React from 'react';
 import Login from './Login';
 import { configure, shallow } from 'enzyme';
 import * as ReactSixteenAdapter from 'enzyme-adapter-react-16';
-import { observer } from 'mobx-react';
-
+ 
 const adapter = ReactSixteenAdapter;
 configure({ adapter: new adapter.default() });
 
 describe('Login', () => {
     const historyMock = { push: jest.fn() }; 
     window.confirm = jest.fn(() => true) // always click 'yes'
-    const login = shallow(observer(<Login history={historyMock} />));
+    const login = shallow(<Login history={historyMock} />);
     it('Login Page render', () => {
         expect(login).toMatchSnapshot();
     });
